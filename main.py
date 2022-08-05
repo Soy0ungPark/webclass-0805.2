06c938a527581f9e6d268ba3ce4ef72b7e17f6c0
soyoung <alwaysoyoung.p@gmail.com>
2022-08-05 16:04:57 +0900

    topic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from re import A
+topics = [
+    {
+        'id':1, 'title':'commit', 'view':100
+    },
+    {'id':2, 'title':'push', 'view':200},
+    {'id':3, 'title':'pull', 'view':50}
+]
 
-
-a=1
-a
-'# hello'
+topics
